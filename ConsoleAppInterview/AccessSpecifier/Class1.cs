a6aa0c9a109884e722e6605d0963967c4a5d695e
hoanganh8499hd <hoanganhpham.rasta@gmail.com>
2025-05-16 14:03:47 +0700

    Add project files.

---
+++ ConsoleAppInterview/AccessSpecifier/Class1.cs
@@ -0,0 +1,22 @@
+﻿namespace AccessSpecifier
+{
+    internal class BaseClass // This class is accessible only within the same assembly
+    {
+        private class PrivateClass // This class is accessible only within the same class
+        {
+        }
+        protected class ProtectedClass // This class is accessible within the same class and derived classes
+        {
+        }
+        public class PublicClass // This class is accessible from any other code
+        {
+        }
+        internal class InternalClass // This class is accessible only within the same assembly
+        {
+        }
+        protected internal class ProtectedInternalClass // This can be accessed within the same assembly or derived classes
+        {
+        }
+    }
+   
+}
